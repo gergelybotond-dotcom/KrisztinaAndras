5fc81ab77a5b41968d39cf2b4c92c95908cea403
Botond Gergely <botond@192.168.1.7>
2026-09-03 00:42:50 +0300

    Update Worker URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Konfigurációs fájl
 const CONFIG = {
-  WORKER_URL: "https://eskuvo-upload-production.gergely-botond.workers.dev",
+  WORKER_URL: "https://krisztinaandras.bassguitar230.workers.dev",
   DRIVE_FOLDER_ID: "1CcpN3MUtGl6OS49gH7_9mE4nHn4vX1ea",
   ADMIN_PASSWORD_HASH: "2cb815be3f1f2e076de7c9efbc0718ad119cbc1c0db2553c7f4a5212d38c8298"
 };
